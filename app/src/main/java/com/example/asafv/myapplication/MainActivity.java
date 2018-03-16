ad4c114d9205846aa67398b61dee4271dbaa6c38
Asaf <asafvm87@gmail.com>
2018-03-16 10:40:33 +0200

    16.03.18

--- app/src/main/java/com/example/asafv/myapplication/MainActivity.java
+++ app/src/main/java/com/example/asafv/myapplication/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.asafv.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    final String TAG = "exe1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Log.d(TAG, "d-onCreate");
+        Log.e(TAG, "e-onCreate");
+        Log.v(TAG, "v-onCreate");
+        Log.w(TAG, "w-onCreate");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
